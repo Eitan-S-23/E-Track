94c4d68c15133bbcde3c72db7733404870f5fd19
eitan <example@ggbond>
2026-09-03 19:17:07 +0800

    P3-7 反证还原(3/4, 重做)：移除 #if 0 阳性样本，预期转绿

--- USER/HAL/HAL_USB.cpp
+++ USER/HAL/HAL_USB.cpp
@@ -1,7 +1,4 @@
 #include "HAL/HAL.h"
-#if 0 /* P3-7 fail-closed probe: backslash include sample, not compiled */
-#include "HAL\HAL.h"
-#endif
 #include "USB_MSC/UsbMsc.h"
 #include "USB_MSC/msc_diskio.h"
 
